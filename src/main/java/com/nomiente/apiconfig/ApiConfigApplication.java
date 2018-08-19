6a17ca052c0fa708f7cb729dbadcb3f481f3accd
Jhon Gil Sepulveda <jhongil96@gmail.com>
2018-08-18 23:56:17 -0500

    Starting

---
+++ src/main/java/com/nomiente/apiconfig/ApiConfigApplication.java
@@ -0,0 +1,12 @@
+package com.nomiente.apiconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConfigApplication.class, args);
+	}
+}
